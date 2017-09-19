2129c5f151d1563df29b918bcba8caca432c1644
victorcesaroni <victor.cesaroni@gmail.com>
2017-09-19 16:17:46 -0300

    Primeiro commit

---
+++ teste.c
@@ -0,0 +1,32 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/crypto.h>
+#include <linux/init.h>
+#include <linux/stat.h>
+
+/* licenca do modulo */
+MODULE_LICENSE("SO B PUCC 2017");
+MODULE_AUTHOR("GRUPO");
+
+/* parametros do modulo */
+static char encryption_key[4096];
+
+module_param(encryption_key, charp, 0000);
+MODULE_PARM_DESC(encryption_key, "Encryption key");
+
+/* init */
+static int __init crypto_init(void) {	
+	printk(KERN_INFO "[CRYPTO] Init with key %s.\n", encryption_key);		
+	return 0;
+}
+
+/* exit */
+static void __exit crypto_exit(void) {
+	printk(KERN_INFO "[CRYPTO] Exit.\n");	
+}
+
+/* definicao das funcoes de init e exit */
+module_int(crypto_init);
+module_exit(crypto_exit);
+
